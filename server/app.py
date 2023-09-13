9013e21c57b2648b86de0772b3c908bffb426bb4
AnthonyLopez <anthonydlr66@gmail.com>
2023-09-13 17:48:55 -0400

    App.py with class Traveler, get request to see all Travelers for now

---
+++ server/app.py
@@ -0,0 +1,34 @@
+import ipdb;
+
+from flask import Flask, make_response, jsonify;
+from flask_sqlalchemy import SQLAlchemy;
+from flask_migrate import Migrate;
+from flask_restful import Api, Resource;
+from flask_cors import CORS;
+
+from models import db, Traveler;
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travelers.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+CORS = (app)
+
+api = Api(app)
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+class Travelers(Resource):
+
+    def get(self):
+        travelers = Traveler.query.all()
+        response_body = []
+        for traveler in travelers:
+            response_body.append(traveler.to_dict())
+        return make_response(jsonify(response_body), 200)
+    
+
+api.add_resource(Travelers, "/travelers")
