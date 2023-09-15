2dfd3fe63fab3319daab781bd039f2c6958ac2fd
AnthonyLopez <anthonydlr66@gmail.com>
2023-09-14 20:38:20 -0400

    Updated Home component with CSS and image/logos

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,28 @@
 import React from "react";
+import SnowBG from "./assets/SnowBG.jpg";
+import Logo1 from "./assets/Logo1.png";
+import Logo2 from "./assets/Logo2.png";
+import ChampionsLogo from "./assets/ChampionsLogo.png";
 
 
 function Home(){
     return (
-        <div className="grid grid-rows-2">
-            <div>
-                The Title
+        <div className="grid grid-rows-8">
+            <div className="text-white flex p-10 justify-center row-span-3">
+                <img className="h-80" src={SnowBG} alt="Snow Escape"/>
             </div>
-            <div className="grid grid-cols-3">
-                <div>
-                    Octo Uno
+            <div className="bg-blue-300 text-center p-5">
+                <span className="text-black text-2xl italic font-semibold">The Sacred Flame Awakens...</span>
+            </div>
+            <div className="grid grid-cols-3 row-span-3">
+                <div className="flex justify-center p-10">
+                    <img className="w-96"src={Logo1} alt="Octopath Traveler"/>
                 </div>
-                <div>
-                    Octo Dos
+                <div className="flex justify-center p-10">
+                    <img className="w-96" src={Logo2} alt="Octopath Traveler II"/>
                 </div>
-                <div>
-                    Champions
+                <div className="flex justify-center p-10">
+                    <img className="w-96" src={ChampionsLogo} alt="Champions Of The Continent"/>
                 </div>
             </div>
         </div>
