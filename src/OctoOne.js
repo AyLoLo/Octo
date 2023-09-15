0308452003e810e9636cfd20b7e1c882e3101da9
AnthonyLopez <anthonydlr66@gmail.com>
2023-09-15 13:08:24 -0400

    Created Traveler component with dummy HTML

--- src/OctoOne.js
+++ src/OctoOne.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-function OctoOne(){
+function OctoOne(travelers){
+
+    
 
     return (
         <div className="text-white">
---
+++ src/Traveler.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Traveler(){
+    
+    return (
+        <div>
+            ????
+        </div>
+    )
+}
+
+export default Traveler;
