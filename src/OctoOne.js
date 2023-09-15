57f640ce1d6027220b6704095805a4a7aba6c5cf
AnthonyLopez <anthonydlr66@gmail.com>
2023-09-15 13:19:47 -0400

    Filtered out travelers from Octopath Traveler then passed down the prop in Traveler component at OctoOne.js

--- src/OctoOne.js
+++ src/OctoOne.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Traveler from "./Traveler";
 
 function OctoOne(travelers){
 
+    const oneTravelers = travelers.filter((traveler) => {
+        if(traveler.game_origin === "Octopath Traveler"){
+            return true 
+        } else {
+            return null 
+        }
+    })
     
+    const oneTraveler = oneTravelers.map(traveler => {
+        return <Traveler traveler={traveler} key={traveler.id}/>
+    })
 
     return (
         <div className="text-white">
-            Hello There
+            {oneTraveler}
         </div>       
     )
 }
