06192bf7167095582716d10739aafe8579216c0a
AnthonyLopez <anthonydlr66@gmail.com>
2023-09-15 14:08:19 -0400

    Updated CSS on OctoOne.js and Traveler.js, Traveler.js currently displaying image and name of travelers

--- src/OctoOne.js
+++ src/OctoOne.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Traveler from "./Traveler";
 
-function OctoOne(travelers){
+function OctoOne({travelers}){
 
     const oneTravelers = travelers.filter((traveler) => {
         if(traveler.game_origin === "Octopath Traveler"){
@@ -16,8 +16,10 @@ function OctoOne(travelers){
     })
 
     return (
-        <div className="text-white">
-            {oneTraveler}
+        <div className="bg-[url('https://images5.alphacoders.com/931/thumb-1920-931888.jpg')] bg-center bg-no-repeat mt-20">    
+            <div className="grid grid-rows-4 grid-cols-2 gap-10 p-10">
+                {oneTraveler}
+            </div>
         </div>       
     )
 }
--- src/Traveler.js
+++ src/Traveler.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-function Traveler(){
+function Traveler({traveler}){
     
     return (
-        <div>
-            ????
+        <div className="md:border-4 border-white flex justify-center items-center bg-black">
+            <div>    
+                <img src={traveler.sprite_image} alt={traveler.first_name}/>
+            </div>
+            <span className="text-white">
+                {traveler.first_name} {traveler.last_name}
+            </span>
         </div>
     )
 }
